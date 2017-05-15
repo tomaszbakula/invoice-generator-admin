c8a3edb980e03bcc01d78116bddc9db301031d27
Tomasz Bakula <tbakula92@gmail.com>
2017-05-15 18:16:10 +0100

    Get invoice endpoint.

--- controllers/invoice.js
+++ controllers/invoice.js
@@ -1,15 +1,17 @@
-var Invocie = require('../models/invoice');
+var Invoice = require('../models/invoice');
 
 /* Preload invoice data method. */
 function load(req, res, next, id) {
-  Invoice.findById(id).exec(function (err, invoice) {
-    req.dbInvoice = invoice;
-  });
+  Invoice.findOne({ _id: id, userId: req.user.id })
+    .then(invoice => {
+      req.dbInvoice = invoice;
+      next();
+    }, err => next(err));
 }
 
 /* List all invoices. */
 function list(req, res, next) {
-  Invocie.find({ userId: req.user.id })
+  Invoice.find({ userId: req.user.id })
   .exec((err, invoices) => {
     if (err) { return next(err); }
     res.json(invoices);
@@ -21,13 +23,17 @@ function create(req, res, next) {
   // Add user id.
   req.body.userId = req.user.id
 
-  Invocie.create(req.body)
-  .then((savedInvoice) => {
+  Invoice.create(req.body)
+  .then(savedInvoice => {
     return res.json(savedInvoice);
-  }, (err) => next(err));
+  }, err => next(err));
 }
 
-function get(req, res, next) {}
+/* Get invoice. */
+function get(req, res, next) {
+  return res.json(req.dbInvoice);
+}
+
 function update(req, res, next) {}
 function remove(req, res, next) {}
 
